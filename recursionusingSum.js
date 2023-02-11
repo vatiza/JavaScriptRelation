d15fd8b9a450bf07eaadd3597a9fc264713f20f0
vatiza <rakibur.mac@gmail.com>
2023-02-12 03:54:16 +0600

    add recursion

---
+++ recursionusingSum.js
@@ -0,0 +1,10 @@
+// let sum = 0;
+// for (let i = 0; i <= 5; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+let sum = 0;
+for (let i = 5; i >= 1; i--){
+    sum = sum + i;
+}
+console.log(sum);
