e147a573694dbbb918c9849be87a7ff31c84f702
vatiza <rakibur.mac@gmail.com>
2023-02-12 04:18:04 +0600

    add objecj.js

--- recursionusingSum.js
+++ recursionusingSum.js
@@ -3,8 +3,17 @@
 //     sum = sum + i;
 // }
 // console.log(sum);
-let sum = 0;
-for (let i = 5; i >= 1; i--){
-    sum = sum + i;
+// let sum = 0;
+// for (let i = 5; i >= 1; i--){
+//     sum = sum + i;
+// }
+// console.log(sum);
+function sum(i) {
+    if (i == 1) {
+        return 1;
+    }
+    console.log(i);
+    return i + sum(i - 1);
 }
-console.log(sum);
+const result = sum(5);
+console.log(result);
